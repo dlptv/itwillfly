134e74f08f40bdc1a3f8f983bd3cba00a37cc962
Dmitrii Laptev <dmitrii.laptev@merck.com>
2017-12-31 18:40:19 +0300

    added vincenty support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 docker build -t itwillfly-build .
 rm -rf deploy
 mkdir deploy
-docker run --rm itwillfly-build cat itwillfly.tar.gz > deploy/itwillfly.tar.gz
+docker run -v ${PWD}/deploy:/usr/src/deploy --rm itwillfly-build tar czf ../deploy/itwillfly.tar.gz --exclude=itwillfly.tar.gz --exclude .git . 
+scp -P 5555 deploy/itwillfly.tar.gz root@dlap.me:itwillfly/itwillfly.tar.gz
+ssh root@dlap.me -p 5555 'cd itwillfly && tar xzf itwillfly.tar.gz'
